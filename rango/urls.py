9c31368a395b81a14b1838a9a7319cba3ebf193b
2079532k-laptop <bkav93@gmail.com>
2015-03-09 14:16:03 +0000

    chapters 16 and 17, done.

--- rango/urls.py
+++ rango/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     #url(r'^register/$', views.register, name='register'),
     #url(r'^login/$', views.user_login, name='login'),
     url(r'^restricted/', views.restricted, name='restricted'),
-    url(r'^search/', views.search, name='search')
+    url(r'^search/', views.search, name='search'),
+    url(r'^goto/$', views.track_url, name='goto')
     )
--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
-from rango.models import Category, Page
-from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from models import Category, Page, UserProfile
+from forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
-from rango.bing_search import run_query
+from bing_search import run_query
+
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
 
 
 def index(request):
@@ -16,73 +30,79 @@ def index(request):
 
     context_dict = {'categories': category_list, 'pages': page_list}
 
-    visits = request.session.get('visits')
-    if not visits:
-        visits = 1
-    reset_last_visit_time = False
+    try:
+        visits = request.session.get('visits')
+        if not visits:
+            visits = 1
+        reset_last_visit_time = False
 
-    last_visit = request.session.get('last_visit')
-    if last_visit:
-        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+        last_visit = request.session.get('last_visit')
+        if last_visit:
+            last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
 
-        if (datetime.now() - last_visit_time).seconds > 0:
-            # ...reassign the value of the cookie to +1 of what it was before...
-            visits = visits + 1
-            # ...and update the last visit cookie, too.
+            if (datetime.now() - last_visit_time).seconds > 0:
+                # ...reassign the value of the cookie to +1 of what it was before...
+                visits = visits + 1
+                # ...and update the last visit cookie, too.
+                reset_last_visit_time = True
+        else:
+            # Cookie last_visit doesn't exist, so create it to the current date/time.
             reset_last_visit_time = True
-    else:
-        # Cookie last_visit doesn't exist, so create it to the current date/time.
-        reset_last_visit_time = True
 
-    if reset_last_visit_time:
-        request.session['last_visit'] = str(datetime.now())
-        request.session['visits'] = visits
-    context_dict['visits'] = visits
-
+        if reset_last_visit_time:
+            request.session['last_visit'] = str(datetime.now())
+            request.session['visits'] = visits
+        context_dict['visits'] = visits
+    except:
+        pass
 
     response = render(request,'rango/index.html', context_dict)
 
     return response
+
+#    def index(request):
+#        return HttpResponse("Rango says hey there world!<br/> <a href='/rango/about'>About</a>")
+
 
 def about(request):
     visits = request.session.get('visits')
     if not visits:
         visits = 1
     reset_last_visit_time = False
-    context_dict = {'boldmessage': "", 'visits':visits}
-
+    context_dict = {'boldmessage': "This tutorial has been completed by me, Jacob '2091723' Ford.",
+                    'visits':visits}
     return render(request, 'rango/about.html', context_dict)
 
-def category(request, category_name_slug):
 
-    # Create a context dictionary which we can pass to the template rendering engine.
+def category(request, category_name_slug):
     context_dict = {}
+    context_dict['result_list'] = None
+    context_dict['query'] = None
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+            context_dict['result_list'] = result_list
+            context_dict['query'] = query
 
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # So the .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
-        context_dict['category_name_slug'] = category_name_slug
-
-        # Retrieve all of the associated pages.
-        # Note that filter returns >= 1 model instance.
-        pages = Page.objects.filter(category=category)
-
-        # Adds our results list to the template context under name pages.
+        pages = Page.objects.filter(category=category).order_by('-views')
         context_dict['pages'] = pages
-        # We also add the category object from the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
     except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything - the template displays the "no category" message for us.
         pass
 
-    # Go render the response and return it to the client.
+    if not context_dict['query']:
+        context_dict['query'] = category.name
+
     return render(request, 'rango/category.html', context_dict)
 
+@login_required
 def add_category(request):
     # A HTTP POST?
     if request.method == 'POST':
@@ -107,23 +127,32 @@ def add_category(request):
     # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
 
     try:
         cat = Category.objects.get(slug=category_name_slug)
+        print "okay1"
     except Category.DoesNotExist:
                 cat = None
 
     if request.method == 'POST':
+        print "okay2"
         form = PageForm(request.POST)
+        print "okay3"
         if form.is_valid():
             if cat:
+                print "okay4"
                 page = form.save(commit=False)
                 page.category = cat
                 page.views = 0
                 page.save()
+                print "okay5"
                 # probably better to use a redirect here.
-                return category(request, category_name_slug)
+                try:
+                    return category(request, category_name_slug)
+                except:
+                    return redirect('index')
         else:
             print form.errors
     else:
@@ -133,19 +162,24 @@ def add_page(request, category_name_slug):
 
     return render(request, 'rango/add_page.html', context_dict)
 
+
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html', {})
 
-def search(request):
 
-    result_list = []
+def track_url(request):
+    page_id = None
+    url = '/rango/'
+    if request.method == 'GET':
+        if 'page_id' in request.GET:
+            page_id = request.GET['page_id']
+            try:
+                page = Page.objects.get(id=page_id)
+                page.views = page.views + 1
+                page.save()
+                url = page.url
+            except:
+                pass
 
-    if request.method == 'POST':
-        query = request.POST['query'].strip()
-
-        if query:
-            # Run our Bing function to get the results list!
-            result_list = run_query(query)
-
-    return render(request, 'rango/search.html', {'result_list': result_list})
+    return redirect(url)
